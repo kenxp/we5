86834b8eff6b8730aa9f504e2efd4499ce911995
wlibai <672779@qq.com>
2016-04-17 15:30:26 +0800

    Initial commit

---
+++ data/tpl/web/default/common/footer-base.tpl.php
@@ -0,0 +1,15 @@
+<?php defined('IN_IA') or exit('Access Denied');?>	<script type="text/javascript">
+		require(['bootstrap']);
+		$('.js-clip').each(function(){
+			util.clip(this, $(this).attr('data-url'));
+		});
+	</script>
+	<div class="container-fluid footer" role="footer">
+		<div class="page-header"></div>
+		<span class="pull-left">
+		</span>
+		<span class="pull-right">
+	</div>
+	<?php  if(!empty($_W['setting']['copyright']['statcode'])) { ?><?php  echo $_W['setting']['copyright']['statcode'];?><?php  } ?>
+</body>
+</html>
